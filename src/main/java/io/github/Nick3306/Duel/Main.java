25b8645585aa5caab07f6bd96801302b96ce038b
Nick <nickrebain@yahoo.com>
2015-05-31 23:08:27 -0400

    Initial Creation

---
+++ src/main/java/io/github/Nick3306/Duel/Main.java
@@ -0,0 +1,12 @@
+package io.github.Nick3306.Duel;
+
+import java.util.ArrayList;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin 
+{
+	ArrayList<Duel> duels = new ArrayList<Duel>();
+	ArrayList<Arena> arenas = new ArrayList<Arena>();
+	
+}
